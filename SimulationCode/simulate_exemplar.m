8be86c69e69a8e83f7c9289b2e03340467b89af2
Kate Nussenbaum <katenuss@gmail.com>
2019-08-12 21:08:00 -0400

    added exemplar-based model

---
+++ SimulationCode/simulate_exemplar.m
@@ -0,0 +1,81 @@
+clear
+
+%% Simulate exemplar
+% In this simulation, we assume that agents make predictions by sampling
+% the most similar instance they previously experienced, based on the
+% euclidean distance between Xs
+
+%% generative parameters ==================================================
+% number of dimensions
+D = 5;
+
+% regression coefficients
+beta = [.55; .25; .12; .06; .02];
+
+% generative function for sampling stimuli 
+% note stimulus values are between 0 and 100
+gX = @() (rand(D,1))*100;
+
+% noise function
+nZ = @() 0 * randn(1);
+
+%number of subjects
+%num_subs = 1000;
+
+%% simulate ===============================================================
+% initialize learning algorithm
+theta = rand(D,1);
+
+T = 500;
+
+ for t = 1:T
+     
+    % sample X for this trial
+    X = gX();
+    
+    %compute prediction
+    if t > 1
+        dist = sqrt(sum((x_store - X).^2));
+        Rhat = correct_response_store(dist == min(dist));
+    else
+        Rhat = 50 + 10*randn(1);
+    end
+    
+    %store Rhats
+    Rhat_store(:,t) = Rhat;
+    
+    %store X's
+    x_store(:,t) = X;
+    
+    %add correct response to store
+    correct_response = beta' * X + nZ();
+    correct_response_store(t) = correct_response;
+    
+    %store error
+    error = Rhat - correct_response;
+    error_store(t) = error;
+    
+ end
+
+ %% Plot errors over time
+figure(1); clf;  hold on;
+plot(error_store')
+xlabel('Trial')
+ylabel('Error')
+
+%% Plot regression weights
+thetas = regress(Rhat_store', x_store');
+
+figure
+bar([thetas(1) beta(1); thetas(2) beta(2); thetas(3) beta(3); thetas(4) beta(4); thetas(5) beta(5)]);
+xlabel('Predictor')
+ylabel('Weight')
+legend('Model Prediction', 'True Weight')
+
+
+
+
+
+
+
+
