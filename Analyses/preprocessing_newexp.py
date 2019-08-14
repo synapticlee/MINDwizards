5d6d202c22ff9c37dd839d24a993dd649c19f521
Mingyu <mingyus@princeton.edu>
2019-08-14 17:39:01 -0400

    edit the preprocessing script to process all the data files

--- Analyses/preprocessing_newexp.py
+++ Analyses/preprocessing_newexp.py
@@ -33,7 +33,13 @@ from scipy.stats import linregress
 import scipy as sp
 
 # %%
-subList = ['1cdzsxek92mz66k_2019-08-10', '9fzhnxu5uzmnbuf_2019-08-10']
+# get the list of subjects
+subList = []
+dataFolder = '../Data_equalWeights/'
+allFileNames = os.listdir(dataFolder)
+for fileName in allFileNames:
+    if fileName.startswith('wizard_gems_data'):
+        subList.append(fileName[17:17+15])
 
 # %%
 colNames = ['sub','NTrials','iTrial','correct_response', 'response'] + ['bars'+str(i) for i in np.arange(5)+1] + ['weights'+str(i) for i in np.arange(5)+1] + ['condition', 'RT']
@@ -42,7 +48,7 @@ colNames = ['sub','NTrials','iTrial','correct_response', 'response'] + ['bars'+s
 data = pd.DataFrame()
 
 for iSub, sub in enumerate(subList):
-    dataSub = pd.read_csv('wizard_gems_data_'+sub+'.csv')
+    dataSub = pd.read_csv(dataFolder+'wizard_gems_data_'+sub+'_2019-08-10.csv')
     dataSub = dataSub[dataSub['trial_type'] == 'wizard-gem-trial'].copy().reset_index(drop=True)
     dataSub['sub'] = iSub+1
     dataSub['NTrials']= dataSub.shape[0]
@@ -71,9 +77,9 @@ for iSub, sub in enumerate(subList):
             dataSub.loc[iRow, 'weights'+str(iBar+1)] = float(gem_weights_str[locComma[iBar]+1:locComma[iBar+1]])
     
     dataSub = dataSub[colNames]
-    dataSub.to_csv('preprocessed_data_'+sub+'.csv',index=None)
+    dataSub.to_csv(dataFolder+'preprocessed_data_'+sub+'_2019-08-10.csv',index=None)
     
     data = data.append(dataSub)
 
 # %%
-data.to_csv('preprocessed_data_2019-08-10.csv',index=None)
+data.to_csv(dataFolder+'preprocessed_dataAll_2019-08-10.csv',index=None)
