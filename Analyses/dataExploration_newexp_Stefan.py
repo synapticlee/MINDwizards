edddb32d6bcd3213159e3da36bfe9a030a2e468a
stefanuddenberg <rahonavis75@gmail.com>
2019-08-15 22:39:07 -0400

    PDFs for people without proper software

--- Analyses/dataExploration_newexp_Stefan.py
+++ Analyses/dataExploration_newexp_Stefan.py
@@ -322,11 +322,13 @@ def plot_average_data(data, beta_weights, filename=None):
 
 sorted_beta_weights = np.sort(beta_weights)
 learner_data = data_for_plotting[data_for_plotting["learner"] == 1]
-plot_average_data(learner_data, sorted_beta_weights, filename="new_task_learner.svg")
+plot_average_data(learner_data, sorted_beta_weights, filename="new_task_learner.pdf")
 
 # %%
 non_learner_data = data_for_plotting[data_for_plotting["learner"] == 0]
-plot_average_data(non_learner_data, sorted_beta_weights, filename="new_task_non-learner.svg")
+plot_average_data(non_learner_data, sorted_beta_weights, filename="new_task_non-learner.pdf")
 
 # %%
-plot_average_data(data_for_plotting, sorted_beta_weights, filename="new_task_all_subjects.svg")
+plot_average_data(data_for_plotting, sorted_beta_weights, filename="new_task_all_subjects.pdf")
+
+# %%
